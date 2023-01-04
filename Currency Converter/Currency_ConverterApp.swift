d5186e8a0809c7d91f3b6c048e60080f42482c20
Oleh Titov <oleg.titov81@gmail.com>
2023-01-04 07:29:51 +0100

    Initial Commit

---
+++ Currency Converter/Currency_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ConverterApp.swift
+//  Currency Converter
+//
+//  Created by Oleh Titov on 04.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
